d278040254f8ce773cf4300ec7ed7200b4d2485f
sowmya <sowmyasrinivas239@gmail.com>
2025-01-21 15:38:21 +0530

    added files

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -11,7 +11,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 export const placeOrder = async (req,res)=>{
 
-    const frontend_url= "http://localhost:5174"
+    const frontend_url= "https://food-delivery-frontend-6q20.onrender.com"
     try {
         const newOrder = new orderModel({
             userId:req.body.userId,
